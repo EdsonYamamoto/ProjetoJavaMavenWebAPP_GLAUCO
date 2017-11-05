342e1db46826ac6ddcb1d0df06cf47e54c51f7b8
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2017-11-05 12:03:21 -0200

    teste2

--- src/main/java/entidade/AppTest.java
+++ src/main/java/entidade/AppTest.java
@@ -3,9 +3,9 @@ package entidade;
 public class AppTest {
 	public static void main(String[] args)
 	{
-		Produto p1 = new Produto();
-		Cliente c1 = new Cliente();
+		Produto[] produto = null;
 		Item i1 = new Item();
+		i1.setProduto(produto);
 		//i1.setProduto();
 	}
 }
